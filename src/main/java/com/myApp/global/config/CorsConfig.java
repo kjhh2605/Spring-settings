5d4151bf0d23cef8589c5de587f49d801005e5f2
junho <2171168@hansung.ac.kr>
2025-12-28 16:28:43 +0900

    이슈/pr 템플릿

---
+++ src/main/java/com/myApp/global/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.myApp.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// org.springframework.web.cors.reactive 는 WebFlux
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://your-frontend.com"));
+        config.setAllowCredentials(true);  // 쿠키 전송 허용
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
